680f28906a8e2b33bb8f77945945a1e058dfdd20
Thomas Lowe <thomasalexanderlowe@gmail.com>
2019-12-18 21:25:16 +0000

    Loaded a dataset into the program.

--- ai.py
+++ ai.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-dataFileName = input("Please enter the name of the data file.")
-print(dataFileName)
-data = pd.read_csv(dataFileName)
+#dataFileName = input("Please enter the name of the data file.")
+#print(dataFileName)
+#data = pd.read_csv(dataFileName)
+
+data = pd.read_csv("Stocks/prices.csv")
+print(data)
