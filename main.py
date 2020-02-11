50e20b3aeeac87ed3c61e8f08105da3b82c5849c
Thomas <thomasalexanderlowe@gmail.com>
2020-02-11 10:34:43 +0000

    Editting GUI

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from tkinter import *
 import tkinter as tk
 print("test")
 
 win = tk.Tk()
 
 win.title("Stock Portfolio Optimisation Tool")
-
+lbl = Label(win, text="test")
+lbl.grid(column=0,row=0)
+win.geometry("1280x720")
 #keeps the program running until the window is closed
 win.mainloop()
