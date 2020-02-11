2f27d3a56d26a69e5a966aa7551406982de14841
Thomas <thomasalexanderlowe@gmail.com>
2020-02-11 18:59:20 +0000

    Added classes to the code

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk
 
 #https://likegeeks.com/python-gui-examples-tkinter-tutorial/#Create-your-first-GUI-application
 
 win = Tk()
-
 win.title("Stock Portfolio Optimisation Tool")
-
 win.geometry("1280x720")
 
-enterStock1 = Entry(win,width=10)
-enterStock1.grid(column=1,row=0)
-enterStock2 = Entry(win,width=10)
-enterStock2.grid(column=2,row=0)
-enterStock3 = Entry(win,width=10)
-enterStock3.grid(column=1,row=1)
-enterStock4 = Entry(win,width=10)
-enterStock4.grid(column=2,row=1)
+class App(object):
+    def __init__(self,master):
+        self.master = master
+        self.create_text()
+        self.create_tabs()
 
-enterStock1.focus()
+    def create_text(self):
+
+        enterStock1 = Entry(win,width=10)
+        enterStock1.grid(column=1,row=0)
+        enterStock2 = Entry(win,width=10)
+        enterStock2.grid(column=2,row=0)
+        enterStock3 = Entry(win,width=10)
+        enterStock3.grid(column=1,row=1)
+        enterStock4 = Entry(win,width=10)
+        enterStock4.grid(column=2,row=1)
+
+        enterStock1.focus()
+    
+    def create_tabs(self):
+        tab_control = ttk.Notebook(win)
+        portfolioTab = ttk.Frame(tab_control)
+
+        tab_control.add(portfolioTab, text='Portfolio Optimisation')
+        tab_control.pack(expand=1,fill='both')
+
+
 
 #keeps the program running until the window is closed
+
+app=App(win)
 win.mainloop()
