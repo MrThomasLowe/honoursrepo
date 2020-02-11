e8b4de0ad01e534d65134b8b9104137cf13551c5
Thomas <thomasalexanderlowe@gmail.com>
2020-02-11 10:46:26 +0000

    Changing layout of code

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from tkinter import *
 import tkinter as tk
-print("test")
+root = Tk()
 
-win = tk.Tk()
+enterStock
 
-win.title("Stock Portfolio Optimisation Tool")
-lbl = Label(win, text="test")
+root.title("Stock Portfolio Optimisation Tool")
+lbl = Label(root, text="test")
 lbl.grid(column=0,row=0)
-win.geometry("1280x720")
+root.geometry("1280x720")
+
+enterStock1 = tk.Entry(root)
+root.create_window(200,400,window=enterStock1)
+
 #keeps the program running until the window is closed
-win.mainloop()
+root.mainloop()
