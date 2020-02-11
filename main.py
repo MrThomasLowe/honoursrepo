a9feb4fd3684795ccca55254bb408debd1ed7dde
Thomas <thomasalexanderlowe@gmail.com>
2020-02-11 17:06:45 +0000

    Added text boxes to take user input

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from tkinter import *
 import tkinter as tk
-root = Tk()
+win = Tk()
 
-enterStock
-
-root.title("Stock Portfolio Optimisation Tool")
-lbl = Label(root, text="test")
+win.title("Stock Portfolio Optimisation Tool")
+lbl = Label(win, text="test")
 lbl.grid(column=0,row=0)
-root.geometry("1280x720")
+win.geometry("1280x720")
 
-enterStock1 = tk.Entry(root)
-root.create_window(200,400,window=enterStock1)
+enterStock1 = Entry(win,width=10)
+enterStock1.grid(column=1,row=0)
+enterStock2 = Entry(win,width=10)
+enterStock2.grid(column=2,row=0)
+enterStock3 = Entry(win,width=10)
+enterStock3.grid(column=1,row=1)
+enterStock4 = Entry(win,width=10)
+enterStock4.grid(column=1,row=2)
 
 #keeps the program running until the window is closed
-root.mainloop()
+win.mainloop()
